d431cc913ce457b23d0ed3a95fe80aa9aa6e7205
Robert Kurczak <robertkurczak.studia@gmail.com>
2024-08-10 00:55:42 +0200

    Added packages to install script

--- install.sh
+++ install.sh
@@ -1,34 +1,54 @@
+# ========= Options =========
 opt_auto_confirm=false
 
 while getopts ":y" opt; do
-    case ${opt} in
-        y )
-            opt_auto_confirm=true
-            ;;
-        \? )
-            echo "Invalid option: -$OPTARG" >&2
-            exit 1
-            ;;
-    esac
+	case ${opt} in
+		y )
+			opt_auto_confirm=true
+			;;
+		\? )
+			echo "Invalid option: -$OPTARG" >&2
+			exit 1
+			;;
+	esac
 done
+# ===========================
 
+# ======== Functions ========
 can_place_file() {
-    local file_path="$1"
+	local file_path="$1"
 
-    if [[ "$opt_auto_confirm" = false && -e "$file_path" ]]; then
-        read -p "File $file_path already exists. Do you want to replace it? (y/n): " choice
-        choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
+	if [[ "$opt_auto_confirm" = false && -e "$file_path" ]]; then
+		read -p "File $file_path already exists. Do you want to replace it? (y/n): " choice
+		choice=$(echo "$choice" | tr '[:upper:]' '[:lower:]')
 
-        [[ "$choice" == "y" || "$choice" == "yes" ]]
-        return
-    fi
+		[[ "$choice" == "y" || "$choice" == "yes" ]]
+		return
+	fi
 
-    true
+	true
 }
+# ===========================
 
+# ========= Packages ========
+REQUIRED_PACKAGES=(
+	"hyprland"
+	"alacritty"
+	"waybar"
+	"dunst"
+	"fuzzel"
+)
+
+echo "Installing packages: ${REQUIRED_PACKAGES[@]}"
+
+sudo pacman -S ${REQUIRED_PACKAGES[@]}
+# ===========================
+
+# ========= Configs =========
 source_bashrc_path="$HOME/.config/bash/.bashrc"
 destination_bashrc_path="$HOME/.bashrc"
 
 if can_place_file $destination_bashrc_path; then
-    ln -sf $source_bashrc_path $destination_bashrc_path
+	ln -sf $source_bashrc_path $destination_bashrc_path
 fi
+# ===========================

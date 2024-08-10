ce62be7c7e0ee6a477f6ac5a4c67690cdbc5aece
Robert Kurczak <robertkurczak.studia@gmail.com>
2024-08-10 02:35:43 +0200

    Added grub config with install step

--- install.sh
+++ install.sh
@@ -45,10 +45,28 @@ sudo pacman -S ${REQUIRED_PACKAGES[@]}
 # ===========================
 
 # ========= Configs =========
+# bashrc
 source_bashrc_path="$HOME/.config/bash/.bashrc"
 destination_bashrc_path="$HOME/.bashrc"
 
 if can_place_file $destination_bashrc_path; then
 	ln -sf $source_bashrc_path $destination_bashrc_path
 fi
+
+# grub
+source_grub_path="$HOME/.config/grub/grub"
+destination_grub_path="/etc/default/grub"
+
+if can_place_file $destination_grub_path; then
+	sudo cp $source_grub_path $destination_grub_path
+fi
+
+source_grub_theme_path="$HOME/.config/grub/catppuccin-mocha-grub-theme/"
+destination_grub_theme_path="/usr/share/grub/themes/catppuccin-mocha-grub-theme"
+
+if can_place_file $destination_grub_theme_path; then
+	sudo cp -r $source_grub_theme_path $destination_grub_theme_path
+fi
+
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 # ===========================

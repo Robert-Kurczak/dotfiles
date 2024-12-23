74aa46b4c54cb74d801476a7d586695d7a3d3efc
Robert Kurczak <robertkurczak.studia@gmail.com>
2024-12-23 14:49:20 +0100

    Added hyprland qt utilities to aur packages

--- installer/AUR_PACKAGES.sh
+++ installer/AUR_PACKAGES.sh
@@ -1,5 +1,6 @@
 AUR_PACKAGES=(
 	"catppuccin-gtk-theme-mocha"        # Main GTK theme
 	"hyprshot"                          # Screenshots utility
-    "brother-dcp1610w"                  # Printer driver
+	"brother-dcp1610w"                  # Printer driver
+	"hyprland-qtutils"		    # Hyprland ecosystem utils
 )

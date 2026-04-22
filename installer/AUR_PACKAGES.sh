46b5db636b4c518750049a1ea05c1b055a1a5ca7
Robert <robertkurczak.studia@gmail.com>
2026-04-22 20:40:32 +0200

    Initial setup for nobara

--- installer/AUR_PACKAGES.sh
+++
@@ -1,6 +0,0 @@
-AUR_PACKAGES=(
-	"catppuccin-gtk-theme-mocha"        # Main GTK theme
-	"hyprshot"                          # Screenshots utility
-	"brother-dcp1610w"                  # Printer driver
-	"hyprland-qtutils"		    # Hyprland ecosystem utils
-)
